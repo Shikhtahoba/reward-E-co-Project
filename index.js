8acaa9415aa7ac0de5a9c7a15ae3475c1a3a58a4
shikhtahoba <abdullahalnuman7610@gmail.com>
2024-02-22 22:56:47 -0800

    all commit

---
+++ index.js
@@ -0,0 +1,55 @@
+
+
+let titleCount = 1;
+
+let totalPrice = 0;
+const cards = document.querySelectorAll(".card");
+
+for (let index = 0; index < cards.length; index++) {
+  const card = cards[index];
+
+  card.addEventListener("click",function(){
+
+    const title = card.querySelector("h3").innerText;
+
+    const price = parseFloat(card.querySelector("span").innerText.split(" ")[1]);
+
+    const titleContainer = document.getElementById("title-container");
+
+    const p =document.createElement("p");
+
+    p.innerText =titleCount + "." + title;
+
+    titleContainer.appendChild(p)
+
+    titleCount++;
+    totalPrice += price;
+
+    document.getElementById("totalPrice").innerText = totalPrice.toFixed(2);
+  });
+  
+}
+
+const btn = document.getElementById("apply-btn");
+btn.addEventListener('click',function(){
+  const couponElement  = document.getElementById("input-field").value;
+  const couponCode =couponElement.split(" ").join("").toUpperCase();
+  if(totalPrice >=200){
+
+    if(couponCode === "SELL200"){
+       const discountElement = document.getElementById("discountPrice");
+       const discountAmount = totalPrice * 0.2;
+       discountElement.innerText = discountAmount.toFixed(2);
+
+       const restTotal = document.getElementById("total");
+       restTotal.innerText = totalPrice - discountAmount.toFixed(2);
+
+
+    }else{
+      alert('invalid')
+    }
+
+  }else{
+    alert("please at lest");
+  }
+});
